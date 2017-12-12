0b70f39fd49fd5e9a71f605d632141731d2de6a3
Ldeems <luke.deems@yahoo.com>
2017-12-12 09:28:00 -0500

    getting things set up and ready to start on my new tic, and took my notest to remember on my luke_try_it_page

---
+++ luke_try_it_page.rb
@@ -0,0 +1,4 @@
+#some simple notes to start and get my thinking process out there, I am wanting to make a multi D board
+#and try to use a counter system to pull out the possitions to check for a win
+#need to figure out how exactly to gennerate the board
+#challenge rules are 12line functions and try to comment on most everything.
